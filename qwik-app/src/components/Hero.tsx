391889f1477b5f60c2aaf5d661da50c626ea3dad
Samira <samira.rahimi1998@gmail.com>
2023-05-06 13:33:51 -0400

    qwik

---
+++ qwik-app/src/components/Hero.tsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div class="">Hi</div>;
+};
+
+export default Hero;
---
+++ qwik-app/src/routes/about/index.tsx
@@ -0,0 +1,7 @@
+import { component$ } from "@builder.io/qwik";
+
+const Index = component$(() => {
+  return <div>About</div>;
+});
+
+export default Index;
